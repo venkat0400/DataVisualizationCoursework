ad2c9411202a4baa030f26be7f314405818dc964
Spencer Apeadjei-Duodu <32403049+adkSpence@users.noreply.github.com>
2024-07-08 13:04:57 +0200

    Updated margins for heatmap

--- dashboard.js
+++ dashboard.js
@@ -341,7 +341,7 @@ function populateHeatmapDropdowns(dimensions) {
 }
 
 function renderHeatmap() {
-    const margin = { top: 50, right: 200, bottom: 50, left: 100 };
+    const margin = { top: 50, right: 200, bottom: 300, left: 50 };
     const width = 800 - margin.left - margin.right;
     const height = 800 - margin.top - margin.bottom;
 
